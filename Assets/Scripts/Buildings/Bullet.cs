ab84823063fbd90b6d26aaafdf85ed7be8d8c127
GlobalBlackout <noblegas38@gmail.com>
2020-01-31 22:30:13 +0100

    Building first test

---
+++ Assets/Scripts/Buildings/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject Target;
+
+    private Rigidbody2D _bulletRB;
+    private float _startXPosition;
+    private float _startYPosition;
+
+    private float _bulletSpeed = 500;
+    void Start()
+    {
+        _startXPosition = transform.position.x;
+        _startYPosition = transform.position.y;
+        _bulletRB = GetComponent<Rigidbody2D>();
+
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        Vector2 ShootingVector = GetDirectionForShooting();
+
+        _bulletRB.AddForce(ShootingVector * _bulletSpeed);
+    }
+
+    private Vector2 GetDirectionForShooting()
+    {
+        return new Vector2(Target.transform.position.x - _startXPosition, Target.transform.position.y - _startYPosition);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
