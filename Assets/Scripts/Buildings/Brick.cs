597402eb3de62e45da2ae6851b865a6e8bfef552
GlobalBlackout <noblegas38@gmail.com>
2020-02-01 22:44:11 +0100

    Some ending

--- Assets/Scripts/Buildings/Brick.cs
+++ Assets/Scripts/Buildings/Brick.cs
@@ -9,6 +9,36 @@ public class Brick : MonoBehaviour
     
     [Range(1, 3)]
     public int BrickLives = 3;
+    [Range(0, 250)]
+    public float BreakForce = 15;
+    [Range(0, 250)]
+    public float BreakTorque = 15;
+
+    private void Start()
+    {
+        GetNearBricks();
+    }
+
+    private void GetNearBricks()
+    {
+        RaycastHit2D rightHit = Physics2D.Raycast(transform.position, Vector2.right);
+        if (rightHit && (rightHit.collider.gameObject.tag == "Brick" || rightHit.collider.gameObject.tag == "Grownd_Brick"))
+        {
+            var Join = gameObject.AddComponent<HingeJoint2D>();
+            Join.connectedBody = rightHit.collider.gameObject.GetComponent<Rigidbody2D>();
+            Join.breakForce = BreakForce;
+            Join.breakTorque = BreakTorque;
+        }
+
+        RaycastHit2D downHit = Physics2D.Raycast(transform.position, Vector2.down);
+        if (downHit && (downHit.collider.gameObject.tag == "Brick" || downHit.collider.gameObject.tag == "Grownd_Brick"))
+        {
+            var Join = gameObject.AddComponent<HingeJoint2D>();
+            Join.connectedBody = downHit.collider.gameObject.GetComponent<Rigidbody2D>();
+            Join.breakForce = BreakForce;
+            Join.breakTorque = BreakTorque;
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
