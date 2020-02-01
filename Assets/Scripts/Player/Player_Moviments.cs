c0e00b0d223bfaf776181b4190273e9f9e23a487
GlobalBlackout <noblegas38@gmail.com>
2020-02-01 11:08:23 +0100

    Improve player movement

---
+++ Assets/Scripts/Player/Player_Moviments.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Moviments : MonoBehaviour
+{
+
+    public float Speed = 20f;
+
+    public float JetpackForce = 1f;
+    public float MaxVelocity = 10;
+    public float Acceleration = 50;
+    public float RemaningForce = 3;
+
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void FixedUpdate()
+    {
+        JatpackMovement();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))       
+            transform.Translate(Vector2.left * Speed * Time.deltaTime);
+        
+
+        if (Input.GetKey(KeyCode.D))     
+            transform.Translate(Vector2.right * Speed * Time.deltaTime);
+        
+    }
+
+    private void JatpackMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (_rb.velocity.y > MaxVelocity)
+                _rb.velocity = new Vector2(_rb.velocity.x, MaxVelocity);
+            else
+                _rb.AddForce(Vector2.up * Acceleration);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+            _rb.velocity = new Vector2(_rb.velocity.x, RemaningForce);
+    }
+}
---
+++ Assets/Scripts/Player/SpawnCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour
+{
+    public GameObject Cube;
+
+    private Vector2 _mosePosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _mosePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector2.Distance(transform.position, _mosePosition) < 3f)
+            {
+                Instantiate(Cube, new Vector3(_mosePosition.x, _mosePosition.y, -5), Quaternion.identity);
+            }
+        }
+    }
+}
