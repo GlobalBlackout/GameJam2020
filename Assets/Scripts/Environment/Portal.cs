dffd0c7321ba57fd917838c447b569749e00e5db
GlobalBlackout <noblegas38@gmail.com>
2020-02-02 14:10:57 +0100

    Wip

--- Assets/Scripts/Environment/Portal.cs
+++ Assets/Scripts/Environment/Portal.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public GameObject PortalToTP;
-    public GameObject Player;
+
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            float unitToAddPlayer = Player.transform.position.x > 0 ? 2 : -2;
-            Player.transform.position = new Vector3(PortalToTP.transform.position.x + unitToAddPlayer, Player.transform.position.y, 0);
+            float unitToAddPlayer = _player.transform.position.x > 0 ? 2 : -2;
+            _player.transform.position = new Vector3(PortalToTP.transform.position.x + unitToAddPlayer, _player.transform.position.y, 0);
         }
         else if (collision.gameObject.tag != "Bullet")
         {
